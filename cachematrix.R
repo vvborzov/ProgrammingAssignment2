7d3b49491704c1c1b2b0f1a79779c1f190250dea
vvborzov <vvborzov@gmail.com>
2016-01-20 09:49:00 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix
+## creates a special object, which contains square matrix and cached version of matrix inversion
+## to guarantee provide data integrity, access to returned bject data 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invx <- NULL
+    set <- function(y) {
+        x <<- y
+        invx <<- NULL
+    }
+    get <- function() x
+    set.inv <- function(inv) invx <<- inv
+    get.inv <- function() invx
+    list(
+        set = set,
+        get = get,
+        set.inv = set.inv,
+        get.inv = get.inv
+    )
 }
 
 
-## Write a short comment describing this function
+## cacheSolve
+## Description: returns inversion of matrix which is contained in object x, which should be created
+## by function makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invx <- x$get.inv()
+        if(!is.null(invx)){
+            return(invx)
+        }
+        mat <- x$get()
+        mat.inv <- solve(mat, ...) 
+        x$set.inv(mat.inv)
+        mat.inv
 }
